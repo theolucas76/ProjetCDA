a2e877911a2cae250c5e97420dfc096aa373ef89
theolucas76 <theolucas76@gmail.com>
2022-03-16 19:09:26 +0100

    Test UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Enums\Role;
+use App\Models\UserData;
+use App\Models\Users;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $myDirector = $this->generateUsers(new Role(Role::DIRECTOR));
+        DB::table('users')->insert($myDirector->toArray());
+
+        $myDirectorData = $this->generateUsersData(new Role(Role::DIRECTOR));
+        foreach ($myDirectorData as $data) {
+            DB::table('hc_user_data')->insert($data->toArray());
+        }
+
+        $myManager = $this->generateUsers(new Role(Role::MANAGER));
+        DB::table('users')->insert($myManager->toArray());
+
+        $myManagerData = $this->generateUsersData(new Role(Role::MANAGER));
+        foreach ($myManagerData as $data) {
+            DB::table('hc_user_data')->insert($data->toArray());
+        }
+
+        $myEmployee = $this->generateUsers(new Role(Role::EMPLOYEE));
+        DB::table('users')->insert($myEmployee->toArray());
+
+        $myEmployeeData = $this->generateUsersData(new Role(Role::EMPLOYEE));
+        foreach ($myEmployeeData as $data) {
+            DB::table('hc_user_data')->insert($data->toArray());
+        }
+
+        $myCustomer = $this->generateUsers(new Role(Role::CUSTOMER));
+        DB::table('users')->insert($myCustomer->toArray());
+
+        $myCustomerData = $this->generateUsersData(new Role(Role::CUSTOMER));
+        foreach ($myCustomerData as $data) {
+            DB::table('hc_user_data')->insert($data->toArray());
+        }
+    }
+
+    public function generateUsers(Role $role): Users
+    {
+        $myUser = new Users();
+        switch ($role) {
+            case Role::DIRECTOR:
+                $myUser->setLogin('director');
+                $myUser->setPassword(Hash::make('director'));
+                $myUser->setRole($role);
+                break;
+            case Role::MANAGER:
+                $myUser->setLogin('manager');
+                $myUser->setPassword(Hash::make('manager'));
+                $myUser->setRole($role);
+                break;
+            case Role::EMPLOYEE:
+                $myUser->setLogin('employee');
+                $myUser->setPassword(Hash::make('employee'));
+                $myUser->setRole($role);
+                break;
+            case Role::CUSTOMER:
+                $myUser->setLogin('customer');
+                $myUser->setPassword(Hash::make('customer'));
+                $myUser->setRole($role);
+                break;
+        }
+        return $myUser;
+    }
+
+    public function generateUsersData(Role $role): array {
+
+        $myUserDataName = new UserData();
+        $myUserDataFirstname = new UserData();
+        $myArray = array();
+        switch ($role) {
+            case Role::DIRECTOR:
+                $myUserDataName->setUserId(1);
+                $myUserDataName->setUserDataKey('Nom');
+                $myUserDataName->setUserDataColumn('Director');
+                $myUserDataFirstname->setUserId(1);
+                $myUserDataFirstname->setUserDataKey('Prénom');
+                $myUserDataFirstname->setUserDataColumn('Super');
+                $myArray = [$myUserDataName, $myUserDataFirstname];
+                break;
+            case Role::MANAGER:
+                $myUserDataName->setUserId(2);
+                $myUserDataName->setUserDataKey('Nom');
+                $myUserDataName->setUserDataColumn('Manager');
+                $myUserDataFirstname->setUserId(2);
+                $myUserDataFirstname->setUserDataKey('Prénom');
+                $myUserDataFirstname->setUserDataColumn('Admin');
+                $myArray = [$myUserDataName, $myUserDataFirstname];
+                break;
+            case Role::EMPLOYEE:
+                $myUserDataName->setUserId(3);
+                $myUserDataName->setUserDataKey('Nom');
+                $myUserDataName->setUserDataColumn('Employee');
+                $myUserDataFirstname->setUserId(3);
+                $myUserDataFirstname->setUserDataKey('Prénom');
+                $myUserDataFirstname->setUserDataColumn('Writer');
+                $myArray = [$myUserDataName, $myUserDataFirstname];
+                break;
+            case Role::CUSTOMER:
+                $myUserDataName->setUserId(4);
+                $myUserDataName->setUserDataKey('Nom');
+                $myUserDataName->setUserDataColumn('Customer');
+                $myUserDataFirstname->setUserId(4);
+                $myUserDataFirstname->setUserDataKey('Prénom');
+                $myUserDataFirstname->setUserDataColumn('Viewer');
+                $myArray = [$myUserDataName, $myUserDataFirstname];
+                break;
+        }
+        return $myArray;
+    }
+}
