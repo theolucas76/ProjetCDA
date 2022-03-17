7fee5e5b100e6f6e46441d67aa059d81e0d8497a
theolucas76 <theolucas76@gmail.com>
2022-03-17 18:48:26 +0100

    Improvement: Change naming convention ok

--- app/Models/UserData.php
+++ app/Models/UserData.php
@@ -8,22 +8,21 @@ use \Illuminate\Database\Eloquent\Model;
 class UserData extends Model
 {
     protected $fillable = [
-        'userData_id', 'userData_userId', 'userData_key', 'userData_column'
+        'data_id', 'data_user_id', 'data_key', 'data_column'
     ];
 
-
-    private int $userData_id;
-    private int $userData_userId;
-    private string $userData_key;
-    private string $userData_column;
+    private int $data_id;
+    private int $data_user_id;
+    private string $data_key;
+    private string $data_column;
 
     public function __construct()
     {
         parent::__construct();
         $this->setId(0);
         $this->setUserId(0);
-        $this->setUserDataKey('');
-        $this->setUserDataColumn('');
+        $this->setDataKey('');
+        $this->setDataColumn('');
     }
 
     /**
@@ -31,14 +30,14 @@ class UserData extends Model
      * @return UserData
      */
     public function setId(int $id): UserData {
-        $this->userData_id = $id;
+        $this->data_id = $id;
         return $this;
     }
     /**
      * @return int
      */
     public function getId(): int {
-        return $this->userData_id;
+        return $this->data_id;
     }
 
     /**
@@ -46,7 +45,7 @@ class UserData extends Model
      * @return UserData
      */
     public function setUserId(int $userId): UserData {
-        $this->userData_userId = $userId;
+        $this->data_user_id = $userId;
         return $this;
     }
 
@@ -54,48 +53,48 @@ class UserData extends Model
      * @return int
      */
     public function getUserId(): int {
-        return $this->userData_userId;
+        return $this->data_user_id;
     }
 
     /**
      * @param string $key
      * @return UserData
      */
-    public function setUserDataKey(string $key): UserData {
-        $this->userData_key = $key;
+    public function setDataKey(string $key): UserData {
+        $this->data_key = $key;
         return $this;
     }
 
     /**
      * @return string
      */
-    public function getUserDataKey(): string {
-        return $this->userData_key;
+    public function getDataKey(): string {
+        return $this->data_key;
     }
 
     /**
      * @param string $column
      * @return UserData
      */
-    public function setUserDataColumn(string $column): UserData {
-        $this->userData_column = $column;
+    public function setDataColumn(string $column): UserData {
+        $this->data_column = $column;
         return $this;
     }
 
     /**
      * @return string
      */
-    public function getUserDataColumn(): string {
-        return $this->userData_column;
+    public function getDataColumn(): string {
+        return $this->data_column;
     }
 
     public function toArray(): array
     {
         return array(
-            Keys::DATABASE_USER_DATA_ID => $this->getId(),
+            Keys::DATABASE_DATA_ID => $this->getId(),
             Keys::DATABASE_USER_DATA_USER_ID => $this->getUserId(),
-            Keys::DATABASE_USER_DATA_KEY => $this->getUserDataKey(),
-            Keys::DATABASE_USER_DATA_COLUMN => $this->getUserDataColumn()
+            Keys::DATABASE_DATA_KEY => $this->getDataKey(),
+            Keys::DATABASE_DATA_COLUMN => $this->getDataColumn()
         );
     }
 
